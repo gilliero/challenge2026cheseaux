8ab0e984973a1664f95b464cb5210d0c2e516b94
Grégory Gilliéron <ggillieron@lff.swiss>
2024-10-22 10:04:21 +0200

    first commit

---
+++ JS/HOME/Home.js
@@ -0,0 +1,22 @@
+// Définir la date cible
+const countDownDate = new Date("Jul 15, 2026 16:00:00").getTime();
+
+const countdownFunction = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if (distance < 0) {
+        clearInterval(countdownFunction);
+        document.getElementById("timer").innerHTML = "Le Challenge a commencé!";
+    }
+}, 1000);
