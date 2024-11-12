8ed81722434a34f0a28b4bfa369da9817b16489f
Grégory Gilliéron <ggillieron@lff.swiss>
2024-11-12 16:27:15 +0100

    ok?

--- template/header.js
+++ template/header.js
@@ -1,16 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.querySelector(".menu-toggle button");
-    const dropdownContent = document.querySelector(".dropdown-content");
+document.getElementById('menuButton').addEventListener('click', function() {
+    var dropdownContent = document.getElementById('dropdownContent');
+    dropdownContent.classList.toggle('show');
+});
 
-    menuToggle.addEventListener("click", function() {
-      // Basculer l'affichage du menu avec la classe .show
-      dropdownContent.classList.toggle("show");
-
-      // Basculer entre icônes menu et croix
-      if (dropdownContent.classList.contains("show")) {
-        menuToggle.textContent = 'x'; // Icône de croix
-      } else {
-        menuToggle.textContent = '☰'; // Icône de menu
-      }
-    });
-  });
